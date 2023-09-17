058b3a751701ad3a2efa150ee00d5444bc89fd4d
barat95 <57249252+barat95@users.noreply.github.com>
2023-09-17 06:57:12 +0000

    Update game.test.js

--- __test__/game.test.js
+++ __test__/game.test.js
@@ -1,7 +1,7 @@
-function add(a, b) {
-  return a + b;
+function add (a, b) {
+  return a + b
 }
 
 test('Adding 1 + 2 should equal 3', () => {
-  expect(add(1, 2)).toBe(3);
-});
+  expect(add(1, 2)).toBe(3)
+})
